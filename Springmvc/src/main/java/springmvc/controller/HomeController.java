01ff347a6b5d908e92c147e0365d3cd8bd24ee3b
rm1710 <d290mishraritesh@gmail.com>
2024-08-14 19:53:37 +0530

    new update

--- Springmvc/src/main/java/springmvc/controller/HomeController.java
+++ Springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -1,14 +1,14 @@
 package springmvc.controller;
 
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/home")
-	public String home() {
-		System.out.println("this is home url");
-		return "index";
+	public static void main(String arg[]) {
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("com/becoder/web.xml");
+		System.out.println(context);
 	}
+	
 }
